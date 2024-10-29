9b11c2c0046f51cc7e07e488c9c5d3d2e58dcc74
nima <nima.babanezhad.arimi@gmail.com>
2024-10-29 08:31:48 +0330

    Remove BaseName

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.Fragment>
-    <BrowserRouter basename="/NewLanding">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.Fragment>
